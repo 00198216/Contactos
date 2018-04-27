32bc718850b27e6c7d3e9a0191b8e9bb1c45b2cc
Sebastian <charlzbach14@gmail.com>
2018-04-27 15:28:04 -0600

    Added Searchbar v3

--- app/src/main/java/com/example/charl/contactos/MainActivity.java
+++ app/src/main/java/com/example/charl/contactos/MainActivity.java
@@ -68,12 +68,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
 
 
