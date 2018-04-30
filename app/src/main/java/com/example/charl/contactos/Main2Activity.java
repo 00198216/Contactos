edce2cb1e31d684c80f1f82e9dce00773c3fae78
Sebastian <charlzbach14@gmail.com>
2018-04-30 00:11:12 -0600

    bug fixes

--- app/src/main/java/com/example/charl/contactos/Main2Activity.java
+++ app/src/main/java/com/example/charl/contactos/Main2Activity.java
@@ -6,13 +6,17 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
     Uri imgu;
     ImageView imgv;
+    TextView calen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,7 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         imgv = (ImageView)findViewById(R.id.img);
+        calen= findViewById(R.id.cal);
 
         imgv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,6 +32,29 @@ public class Main2Activity extends AppCompatActivity {
                 openGallery();
             }
         });
+
+
+
+        calen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                "Elige el cumpleaños", Toast.LENGTH_SHORT);
+
+                toast1.show();
+
+                Intent add = new Intent(getApplicationContext(),calendario.class);
+                startActivityForResult(add,1);
+
+
+
+            }
+        });
+        
+
+
+
     }
 
     private void openGallery(){
--- app/src/main/java/com/example/charl/contactos/calendario.java
+++ app/src/main/java/com/example/charl/contactos/calendario.java
@@ -1,13 +1,40 @@
 package com.example.charl.contactos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class calendario extends AppCompatActivity {
+
+    CalendarView Calendario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendario);
+
+        Calendario= findViewById(R.id.cal);
+
+        Calendario.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override public void onSelectedDayChange(
+                    CalendarView view,
+                    int          year,
+                    int          month,
+                    int          dayOfMonth ) {
+
+                String Fecha = ""+dayOfMonth+"/"+""+month+"/"+""+year;
+                Intent sendIntent = new Intent(getApplicationContext(),Main2Activity.class);
+                sendIntent.putExtra("Fecha",Fecha);
+                setResult(Activity.RESULT_OK,sendIntent);
+                finish();
+            }
+
+        });
+
+
+
     }
+
 }
